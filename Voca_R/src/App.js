00b20bf485930bfc72491b21c8c4843340ab7882
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-03-27 20:24:15 +0900

    Add files via upload

--- Voca_R/src/App.js
+++ Voca_R/src/App.js
@@ -7,7 +7,8 @@ import styles from "./App.module.css";
 function App() {
   return <div className="App">
       <Hello />
-    <div className={styles.box}>App</div>
+      <Hello />
+      <Hello />
     </div>
 }
 
