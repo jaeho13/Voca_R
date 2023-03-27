d606f4326f144153173c80e909c9c10c17c70c1b
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-03-27 20:54:40 +0900

    Add files via upload

--- Voca_R/src/App.js
+++ Voca_R/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Hello from './Component/Hello';
-import Welcome from './Component/Welcome';
-import styles from "./App.module.css";
-
 
 function App() {
-  return <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
+  return (
+  <div className="App">
+    <h3>props : properties</h3>
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
+  );
 }
 
 export default App;
