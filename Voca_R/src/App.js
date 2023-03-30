0b892e5df39b89c4bbe5c959f923f21a97002139
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-03-30 23:52:25 +0900

    Add files via upload

--- Voca_R/src/App.js
+++ Voca_R/src/App.js
@@ -1,13 +1,24 @@
 import DayList from "./Component/DayList";
 import Header from "./Component/Header";
 import Day from "./Component/Day";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import EmptyPage from "./Component/EmptyPage";
 
 function App() {
   return (
   <div className="App">
     <Header />
-    <DayList />
-    <Day />
+    <Switch>
+      <Route path="/"> 
+        <DayList />
+      </Route>
+      <Route path="/day/:day">
+        <Day />
+      </Route>
+      <Route>
+        <EmptyPage />
+      </Route>
+    </Switch>
   </div>
   )
 }
--- Voca_R/src/Component/Day.js
+++ Voca_R/src/Component/Day.js
@@ -1,13 +1,13 @@
 import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
 
 export default function Day() {
-    
-    const day = 3;
-    const wordList = dummy.words.filter(word => word.day === day);
-    console.log(wordList);
+    const { day } = useParams();
+    const wordList = dummy.words.filter(word => word.day === Number(day));
 
     return (
         <>
+            <h2>Day {day}</h2>
             <table>
                 <tbody>
                     {wordList.map(word => (
